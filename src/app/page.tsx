7a2fe5892b411ea013a74ad569ca7b74be85dd44
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2025-06-19 13:08:04 +0300

    #add - dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
-import Link from "next/link";
-
-import { ContentLayout } from "$shared/layouts/content-layout";
-import { HomePage } from "$views/home";
+import { HomePage } from "$views/home"
 
 export default function Home() {
-	return (
-		<>
-			<HomePage />
-			<ContentLayout>
-				HOME <Link href={"/test"}>TEST</Link>{" "}
-				<Link href={"/test#form"}>TEST FORM</Link>{" "}
-				<Link href={"/test/images"}>IMAGES</Link>{" "}
-				<Link href={"/test/slug123"}>CUSTOM PAGE</Link>{" "}
-			</ContentLayout>
-		</>
-	);
+  return <HomePage />
 }
