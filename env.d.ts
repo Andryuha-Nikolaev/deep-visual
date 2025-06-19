dcf574160bb43bdd020ad06e732a8f1656deed81
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2025-06-19 12:23:12 +0300

    #add - initial project

---
+++ env.d.ts
@@ -0,0 +1,9 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_BUILD_PROFILE: string;
+    NEXT_PUBLIC_SITE_URL: string;
+    NEXT_PUBLIC_API_URL: string;
+    NEXT_PUBLIC_YANDEX_METRIKA_ENABLED: string;
+    NEXT_PUBLIC_YANDEX_METRIKA_KEY: string;
+  }
+}
