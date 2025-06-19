34c573fe188bace036116693c8fe6e10b1c1f536
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2025-06-19 16:27:49 +0300

    #add - hero

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./styles/index.scss"
 import type { Metadata, Viewport } from "next"
 import NextTopLoader from "nextjs-toploader"
 
-import { primaryFont } from "./fonts"
+import { primaryFont, secondaryFont } from "./fonts"
 import { Providers } from "./providers"
 
 import "simplebar-react/dist/simplebar.min.css"
@@ -18,6 +18,7 @@ import { Analytics } from "$widgets/analytics"
 import { Burger } from "$widgets/burger"
 import { Footer } from "$widgets/footer"
 import { Header } from "$widgets/header"
+import clsx from "clsx"
 
 export const metadata: Metadata = metaConstants
 
@@ -25,13 +26,13 @@ export const viewport: Viewport = viewportConstants
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ru" suppressHydrationWarning={siteConstants.IS_TELEGRAM_WEB_APP}>
+    <html lang="ru" className="dark" suppressHydrationWarning={siteConstants.IS_TELEGRAM_WEB_APP}>
       {siteConstants.IS_TELEGRAM_WEB_APP && (
         <head>
           <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
         </head>
       )}
-      <body className={primaryFont.variable}>
+      <body className={clsx(primaryFont.variable, secondaryFont.variable)}>
         <NextTopLoader color={siteConstants.PROGRESS_BAR_COLOR} showSpinner={false} />
         <Providers>
           <PageLayout>
--- src/views/home/ui/hero/Hero.tsx
+++ src/views/home/ui/hero/Hero.tsx
@@ -1,13 +1,26 @@
-import { ContentLayout } from '$shared/layouts/content-layout';
+import { ContentLayout } from "$shared/layouts/content-layout"
 
-import s from './Hero.module.scss';
+import s from "./Hero.module.scss"
 
 const Hero = () => {
   return (
     <section className={s.block}>
-      <ContentLayout>HERO</ContentLayout>
+      <div className={s.wrap}>
+        <ContentLayout>
+          <div className={s.content}>
+            <p className={s.subtitle}>Превращаем Ваши идеи в&nbsp;реальность</p>
+            <h1 className={s.title}>
+              DeepVisual <span>- нейрофотосессии, нейроконтент</span>
+            </h1>
+            <p className={s.subtitle}>
+              Создаем профессиональный визуальный контент с&nbsp;помощью передовых AI-технологий,
+              мощного железа и&nbsp;ручной доработки
+            </p>
+          </div>
+        </ContentLayout>
+      </div>
     </section>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
