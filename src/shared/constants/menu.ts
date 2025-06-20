b376aa2d21ab8dc7b52a0fa891e04ebd13ac2b17
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2025-06-20 14:47:49 +0300

    #add #hero - bg

---
+++ src/shared/constants/menu.ts
@@ -0,0 +1,21 @@
+import { SectionId } from "./sectionId"
+
+export type MenuItemType = {
+  name: string
+  url: string
+}
+
+export const menu: MenuItemType[] = [
+  {
+    name: "Услуги",
+    url: `/#${SectionId.SERVICES}`,
+  },
+  {
+    name: "Галерея",
+    url: `/#${SectionId.GALLERY}`,
+  },
+  {
+    name: "Контакты",
+    url: `/#${SectionId.CONTACTS}`,
+  },
+]
