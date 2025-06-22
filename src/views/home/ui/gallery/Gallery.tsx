810ad10dab83c2a103815ceaac1421a29d5f1a5c
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2025-06-22 11:44:23 +0300

    #add #gallery - images

--- src/views/home/ui/gallery/Gallery.tsx
+++ src/views/home/ui/gallery/Gallery.tsx
@@ -1,27 +1,41 @@
+"use client";
+
 import { SectionId } from "$shared/constants/sectionId";
 import { SectionLayout } from "$shared/layouts/section-layout";
 import Image from "next/image";
 import { galleryData } from "./config/data";
 import s from "./Gallery.module.scss";
+import { useState } from "react";
+import clsx from "clsx";
 
 export const Gallery = () => {
+  const [openImageUrl, setOpenImageUrl] = useState("");
+
   return (
     <SectionLayout title={galleryData.title} id={SectionId.GALLERY}>
       <div className={s.list}>
         {galleryData.data.map(({ id, image, alt }) => (
-          <div className={s.item} key={id}>
-            <Image src={image} alt="" quality={90} fill className={s.bg} sizes="828px" />
-            <Image
-              src={image}
-              alt={alt}
-              width={2}
-              height={3}
-              className={s.image}
-              quality={90}
-              sizes="828px"
-            />
+          <div className={s.item} key={id} onClick={() => setOpenImageUrl(image)}>
+            <Image src={image} alt="" quality={100} fill className={s.bg} sizes="828px" />
+            <Image src={image} alt={alt} fill className={s.image} quality={100} sizes="828px" />
           </div>
         ))}
+      </div>
+      <div
+        className={clsx(s.modalImageWrap, openImageUrl && s.open)}
+        onClick={() => setOpenImageUrl("")}
+      >
+        {openImageUrl && (
+          <Image
+            className={s.modalImage}
+            src={openImageUrl}
+            alt=""
+            width={1}
+            height={1}
+            sizes="828px"
+            quality={100}
+          />
+        )}
       </div>
     </SectionLayout>
   );
