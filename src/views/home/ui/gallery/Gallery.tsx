112232413399791659d11d4ce8cf58b24ae706fa
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2025-06-22 11:49:49 +0300

    #add #gallery - images

--- src/views/home/ui/gallery/Gallery.tsx
+++ src/views/home/ui/gallery/Gallery.tsx
@@ -7,17 +7,20 @@ import { galleryData } from "./config/data";
 import s from "./Gallery.module.scss";
 import { useState } from "react";
 import clsx from "clsx";
+import useScrollLock from "$shared/hooks/scroll/useScrollLock";
 
 export const Gallery = () => {
   const [openImageUrl, setOpenImageUrl] = useState("");
+
+  useScrollLock(!!openImageUrl);
 
   return (
     <SectionLayout title={galleryData.title} id={SectionId.GALLERY}>
       <div className={s.list}>
         {galleryData.data.map(({ id, image, alt }) => (
           <div className={s.item} key={id} onClick={() => setOpenImageUrl(image)}>
-            <Image src={image} alt="" quality={100} fill className={s.bg} sizes="828px" />
-            <Image src={image} alt={alt} fill className={s.image} quality={100} sizes="828px" />
+            <Image src={image} alt="" quality={100} fill className={s.bg} sizes="1376px" />
+            <Image src={image} alt={alt} fill className={s.image} quality={100} sizes="1376px" />
           </div>
         ))}
       </div>
@@ -32,7 +35,7 @@ export const Gallery = () => {
             alt=""
             width={1}
             height={1}
-            sizes="828px"
+            sizes="1376px"
             quality={100}
           />
         )}
