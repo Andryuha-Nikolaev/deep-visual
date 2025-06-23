6a07288f331d38bec659fe3a8c7f3068e3db8835
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2025-06-23 10:04:08 +0300

    #fix #style

--- src/widgets/carousel/ui/gallery-slide/GallerySlide.tsx
+++ src/widgets/carousel/ui/gallery-slide/GallerySlide.tsx
@@ -1,20 +1,28 @@
 import Image from "next/image";
 import s from "./GallerySlide.module.scss";
+import { useState } from "react";
+import { Spinner } from "$shared/ui/spinner";
 
 type GallerySlideProps = {
   image: string;
 };
 
 export const GallerySlide = ({ image }: GallerySlideProps) => {
+  const [loading, setLoading] = useState(true);
+
   return (
-    <Image
-      className={s.image}
-      src={image}
-      alt=""
-      width={1}
-      height={1}
-      sizes="1376px"
-      quality={90}
-    />
+    <div className={s.block}>
+      {loading && <Spinner className={s.spinner} />}
+      <Image
+        onLoad={() => setLoading(false)}
+        className={s.image}
+        src={image}
+        alt=""
+        width={1}
+        height={1}
+        sizes="1376px"
+        quality={90}
+      />
+    </div>
   );
 };
