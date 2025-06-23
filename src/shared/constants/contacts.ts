fd8dd34559234f144f6377416a1bb5cc6373482d
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2025-06-23 10:38:50 +0300

    #change #contacts

--- src/shared/constants/contacts.ts
+++ src/shared/constants/contacts.ts
@@ -1,6 +1,6 @@
 export const contacts = {
-  TG: "https://t.me/andryuha_nikolaev",
-  EMAIL: "andryuha_nikolaev@mail.ru",
+  TG: "https://t.me/deep_visual",
+  EMAIL: "deep-visual@mail.ru",
   DEVELOP: "https://github.com/Andryuha-Nikolaev",
   DESIGN: "https://vk.com/figma_templates",
 };
