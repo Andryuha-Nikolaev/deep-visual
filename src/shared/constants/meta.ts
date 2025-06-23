10f8b44962756fde8c57a412379062d5016f1a8d
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2025-06-23 23:07:32 +0300

    #fix #seo - web manifest

--- src/shared/constants/meta.ts
+++ src/shared/constants/meta.ts
@@ -10,66 +10,65 @@ const metaConstants: Metadata = {
   },
   description:
     "Генерация изображений нейросетями - создаем реалистичные нейрофото, аватарки, уникальные AI-иллюстрации и контент для соцсетей. Недорого - от 200₽ за штуку.",
-  // manifest: "/web.manifest.json",
+  manifest: "/web.manifest.json",
   icons: {
-    icon: `${baseUrl}/favicon.ico`,
     apple: {
       rel: "apple-touch-icon",
       type: "image/png",
       sizes: "180x180",
-      url: `${baseUrl}/apple-touch-icon.png`,
+      url: "/apple-touch-icon.png",
     },
     other: [
       {
         rel: "shortcut icon",
         type: "image/x-icon",
-        url: `${baseUrl}/favicon.ico`,
+        url: "/favicon.ico",
       },
       {
-        url: `${baseUrl}/favicon.svg`,
-        type: "image/svg+xml",
         rel: "icon",
+        type: "image/svg+xml",
+        url: "/favicon.svg",
       },
       {
         rel: "icon",
         type: "image/png",
         sizes: "32x32",
-        url: `${baseUrl}/favicon-32x32.png`,
+        url: "/favicon-32x32.png",
       },
       {
         rel: "icon",
         type: "image/png",
         sizes: "64x64",
-        url: `${baseUrl}/favicon-64x64.png`,
+        url: "/favicon-64x64.png",
       },
       {
         rel: "icon",
         type: "image/png",
         sizes: "96x96",
-        url: `${baseUrl}/favicon-96x96.png`,
+        url: "/favicon-96x96.png",
       },
       {
         rel: "icon",
         type: "image/png",
         sizes: "128x128",
-        url: `${baseUrl}/favicon-128x128.png`,
+        url: "/favicon-128x128.png",
       },
       {
         rel: "icon",
         type: "image/png",
         sizes: "192x192",
-        url: `${baseUrl}/favicon-192x192.png`,
+        url: "/favicon-192x192.png",
       },
       {
         rel: "icon",
         type: "image/png",
         sizes: "512x512",
-        url: `${baseUrl}/favicon-512x512.png`,
+        url: "/favicon-512x512.png",
       },
     ],
   },
   openGraph: {
-    images: `${process.env["NEXT_PUBLIC_SITE_URL"]}/og-image.jpg`,
+    images: `${baseUrl}/og-image.jpg`,
   },
   other: {
     "yandex-verification": "4b1296e19c33dc31",
